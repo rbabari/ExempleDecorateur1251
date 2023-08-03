9b6018e8915cb5b9d6c7ee14b8ee6702dc076cfb
rbabari <r.babari@gmail.com>
2023-08-03 09:23:30 -0400

    test decorateur

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-        // component / plat à une méthode executer
-
-        // base component / pizza base
-
-        // decorateur / ingredient à un plat "sur"
-
-        // Concrete decorateur / Ognions
-
-
-
+    Plat p1 = new PizzaBase();
+    System.out.println("Pizza de base = " + p1.getPrix());
+    Plat p2 = new Oeuf(new PizzaBase());
+    System.out.println("Pizza avec oeufs = " + p2.getPrix());
+    Plat p3 = new Ognion(new Oeuf(new PizzaBase()));
+    System.out.println("Pizza avec oeufs = " + p3.getPrix());
 
     }
 }
